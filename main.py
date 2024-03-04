95edec835ec16f0e1aad12c67763e693ab5c2830
Alexander-Plunkett <alexander.b.plunkett@gmail.com>
2024-03-04 00:18:54 +0000

    added get_text, word_count and char_count functions

--- main.py
+++ main.py
@@ -1,1 +1,29 @@
-print("hello world")
+def main():
+    book_path = "books/frankenstein.txt"
+    text = get_text(book_path)
+    num_words = word_count(text)
+    print(f"{num_words} words found in the document")
+    characters = char_count(text)
+    print(characters)
+
+def get_text(path):
+    with open(path) as f:
+        return f.read()
+
+def word_count(book):
+    words = book.split()
+    return len(words)
+
+def char_count(book):
+    char_dict = {}
+    lower_string = book.lower()
+    for char in lower_string:
+        if char in char_dict.keys():
+            char_dict[char] += 1
+        else:
+            char_dict[char] = 1
+    return char_dict
+
+
+main()
+    
