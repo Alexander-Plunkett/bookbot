c55c67dca5c90eac157271093901c17f6e273859
Alexander-Plunkett <alexander.b.plunkett@gmail.com>
2025-09-03 10:25:37 +0100

    update main.py to read text file and calculate the word count, add stats.py to house get_word_count function, add .gitignore

---
+++ main.py
@@ -0,0 +1,12 @@
+from stats import get_word_count
+
+def get_book_text(filepath):
+    with open(filepath) as f:
+        book_string = f.read()
+    return book_string
+
+def main():
+    book_text = get_book_text("books/frankenstein.txt")
+    print(f"{get_word_count(book_text)} words found in the document")
+
+main()
---
+++ stats.py
@@ -0,0 +1,3 @@
+def get_word_count(string):
+    word_count = string.split()
+    return len(word_count)
